33bbe324aa2f76bc5479d4f345056e1520a32001
Khaled Salem <k.salem@qcentris.com>
2021-07-04 10:43:55 +0200

    initial commit

---
+++ fresh-install.bat
@@ -0,0 +1,31 @@
+:: Install choco .exe and add choco to PATH
+@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
+
+:: Install all the packages
+:::: Browsers
+choco install googlechrome -fy
+choco install firefox -fy
+
+:::: Text editors / IDEs
+choco install visualstudiocode -fy
+choco install intellijidea-community -fy
+choco install webstorm -fy
+
+:::: Dev tools
+choco install git -fy
+choco install nodejs.install -fy
+choco install zulu -fy
+
+:::: Virtualization
+choco install virtualbox -fy
+choco install vagrant -fy
+choco install docker-desktop -fy
+
+
+:::: Media
+choco install vlc -fy
+
+:::: Utilities + other
+choco install 7zip.install -fy
+choco install office365proplus -fy
+choco install discord -fy
